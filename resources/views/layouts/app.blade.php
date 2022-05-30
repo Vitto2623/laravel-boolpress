66231f115d410c53ecf041c73b74f1280cd57eef
Vitto2623 <vtr.1@icloud.com>
2022-05-30 19:41:02 +0200

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Laravel-API</title>
+    <title>Laravel-BoolPress</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
